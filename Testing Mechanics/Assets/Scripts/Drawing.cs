1cab8d6806d39e60db6743ffb3fab842cafab729
eli.p <eli.p561@stu.nebo.edu>
2020-01-23 11:54:33 -0700

    Progress has been made

---
+++ Testing Mechanics/Assets/Scripts/Drawing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drawing : MonoBehaviour
+{
+    public float forwardInput;
+    public float speed = .1f;
+    public GameObject cock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = cock.transform.position;
+        }
+    }
+}
---
+++ Testing Mechanics/Assets/Scripts/rotatehandle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatehandle : MonoBehaviour
+{
+    public float forwardInput;
+    public float speed = .1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.right* speed * forwardInput);
+    }
+}
