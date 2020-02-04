fac356c3434aa9e72ce7268f609433192610b8d2
eli.p <eli.p561@stu.nebo.edu>
2020-02-04 10:32:47 -0700

    Possible function

---
+++ Testing Mechanics/Assets/Scripts/FirstBinding.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstBinding : MonoBehaviour
+{
+    public string parentFind = "Pommel";
+
+    private GameObject parent = GameObject.Find("Pommel");
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.parent = parent.transform;
+        transform.position = gameObject.transform.parent.position;
+
+        transform.rotation = gameObject.transform.parent.rotation;
+    }
+}
---
+++ Testing Mechanics/Assets/Scripts/Pommel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pommel : MonoBehaviour
+{
+    public GameObject pommel1;
+    public GameObject pommel2;
+    public float switc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.parent = pommel1.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switc = Input.GetAxis("Vertical");
+        if (switc > .5)
+        {
+
+            transform.parent = pommel2.transform;
+            transform.position = pommel2.transform.position;
+        }
+        else if (switc < -.5)
+        {
+
+            transform.parent = pommel1.transform;
+            transform.position = pommel1.transform.position;
+        }
+
+        transform.position = gameObject.transform.parent.position;
+
+        transform.rotation = gameObject.transform.parent.rotation;
+    }
+}
